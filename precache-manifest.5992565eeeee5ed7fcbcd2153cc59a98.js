51a9632981ab4b7cad327c7b76b40029eb8429fd
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-02-26 23:13:03 +0530

    Updates

---
+++ precache-manifest.5992565eeeee5ed7fcbcd2153cc59a98.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7c6431f6d77eb05cc022689107d221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dba759f8756e0795942",
+    "url": "/static/css/main.10731315.chunk.css"
+  },
+  {
+    "revision": "56ed9efc20fb2f491967",
+    "url": "/static/js/2.03f0f6a4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.03f0f6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dba759f8756e0795942",
+    "url": "/static/js/main.0051dc56.chunk.js"
+  },
+  {
+    "revision": "5da38eedb3bf12e89126",
+    "url": "/static/js/runtime-main.408064cc.js"
+  },
+  {
+    "revision": "b3f76588b25a93f8994f460abd8a7a21",
+    "url": "/static/media/chathumini.b3f76588.jpeg"
+  },
+  {
+    "revision": "3139122234c021dc0e3debb06539be95",
+    "url": "/static/media/dcc.31391222.png"
+  },
+  {
+    "revision": "16a41b2587aaf3574dded537ce074111",
+    "url": "/static/media/leo.16a41b25.jpeg"
+  },
+  {
+    "revision": "1dbdd1dafe15f61d304018e49ad49c7d",
+    "url": "/static/media/resume.1dbdd1da.pdf"
+  },
+  {
+    "revision": "d39d58d0841a3cf1ed900991e1ce5ae5",
+    "url": "/static/media/usjp.d39d58d0.png"
+  },
+  {
+    "revision": "b430a3024dafd185815f1ce0ae399683",
+    "url": "/static/media/vikings.b430a302.jpg"
+  },
+  {
+    "revision": "5c2b214ab63365cd743d2961d00308dd",
+    "url": "/static/media/vta.5c2b214a.png"
+  }
+]);
